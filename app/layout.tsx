d482a2c54a362f8bbd23f4e7a41c9b6278b86a64
kamijodev <kamijodev@gmail.com>
2023-06-30 03:27:04 +0900

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className="">{children}</body>
     </html>
   )
 }
